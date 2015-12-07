da2483556103adf3e2986cf9a082c40890ad2211
David Axelrod <daxaxelrod@gmail.com>
2015-12-07 14:30:26 -0500

    trying to reconcile

---
+++ 12-7-15.py
@@ -0,0 +1,71 @@
+hello again
+
+kinectic theory of gasses
+microscopic perspective
+
+ideal gas means
+    no internal structure
+    each molicule is jsut a point mass/ particle
+    carries translational kinetic energy
+    no iteractions ubless participating in purly kinetic energy
+    volume of molicule is negligible
+
+    these are good for low density conditions
+
+    pressure
+        gas molicules are costantly exerting force on the container
+        chapter 19.3
+        pressure = nMvrms^2
+                   _____
+                    3V
+        n is number of moles
+        M is molecular mass
+        V is volume
+        vrms is square root of average value of the speed squared
+
+
+        you can solve for rms speed
+
+        for ideal gas in microscopic view of temperature
+        vrms = root(3rt/M)
+
+        higher temp means higher speed
+        heavier gas molicule leads to lower speed
+
+        mean free path how far to get gas to disperse
+
+        around sea level, molicule will move 100nm before changing direction
+
+
+        can also write in terms of mass and boltzmans constant
+
+        Average Kinetic energy = 3/2 k temperature,
+        where k is the boltzmans constant
+
+        Energy interal = 3/2 NkT = 3/2 n R T
+
+
+        just add delta e to everything to mainipulate in terms of Q - W
+
+        FOR CONSTANT VOLUME, WORK IS 0
+
+        _______________________________
+
+        for constan volume process
+        specific heat at constant volume = 3/2 times ideal gas constant (r)
+
+        -------------------------------
+        for constant pressure process
+        cp - cv  = r
+
+        plus in stuff on line 55
+
+        we get Cp = 5/2 R
+
+        each degree of freedom in rotation contributes about 1/2 KT to average kinetic energy
+
+        for linear translation  k avg = 1/2 mv^2  = 3/2 kT
+
+        for each direction, 1/2 kt
+
+        
