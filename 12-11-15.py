b685bf38c3aba975cc3e97a2235a4c487d69581d
David Axelrod <daxaxelrod@gmail.com>
2015-12-11 14:10:19 -0500

    Thank Frackin god its friday
    Loom all night
    Lets fracking go
    well nah lets discontinue fracking. terrible for the environment

---
+++ 12-11-15.py
@@ -0,0 +1,88 @@
+Lecture notes 12-11-15
+
+###############################################################
+do we need to know conversion factors for pressure
+find all the constants you can and put them on the index cards
+should have numbers that relate to ideal gasses
+###############################################################
+forces tourques momentum vectors wakakaka
+
+Entropy and second law of thermodynamics
+######          #######3    ######  ######
+
+what is a heat engine
+    something that can convert heat to work
+    there is always a working substance (steam, gas)
+    undergoes a cyclic process
+
+
+General representation of a heat engine
+    need a high temperature reservior
+    also need a low tenperature reservoir
+    Heat Qh is absorbed from hot reservoir during each cyclic
+        some heat is converted to work
+        some small ammount of energy gets lost and gets transfered to low temp reservoir
+
+    to explicitly keep track of heat flow directions
+    heat flow to engine +|Qh|
+    heat flow out of the engire -|QL|
+
+    WORK EQUALS |Qh| - |QL|
+
+Heat enginre efficiency
+    defined by work outut divided by heat input
+    Epsilon = Work/ Qh
+    Epsilon = (1 -| Ql /Qh|)
+
+
+EXAMPLE
+    Carnot Engine (the best engine evvvvvaaa)
+        GOOGLE THIS BITCH
+    from a to b first is isothermal expansion
+    heat Qh is absorbed fronm the reservoir
+
+    from b to c adiabatic expansion (doesnt undergo heat exchange)
+        temperature drops to Tl
+
+    from c to d isothermal compression at Tl
+        during compression heat Ql is lost
+
+    from d to a adiabatic compression ( Q = 0 )
+        temperature increases to Temp high
+
+    for the work done during carnot cycle
+        for full cycle delta e is 0 # which means Q is equal to W
+            Change in entropy for full cycle is also 0
+        if q equals w q equals Qh + 0 + Ql + 0
+
+        so work equals qh minus ql
+
+    for the entropy during carnot
+        during isothermal processes
+        change in entropy is just heat over temp
+        Entropy high = Qh / Temp H
+        Entropy low = -|QL| / Temp L
+
+        For adiabatic processes, no heat exchange so no entropy change
+
+        so for thee full cycke the entropy change is Q H/ temp H = Ql over Temp L
+
+        Temp L/ Temp H = Q l / Q h
+
+        so Epislon, specific for carnot cycle is 1 - Temp low/ temp high
+
+        HW problem
+        work done is area of box
+
+for the index cards
+    for Constant pressure process
+        Q = N*Cv* delta T
+        for monatomic ideal gaas
+            Cv is 3/2 R
+    for Constant pressure
+        cp is 5/2 R
+        Q = N*Cp* delta T
+
+for noncarnot cycle
+    epsilon = Work / Qh
+    
