171f272bd57c7989f999dc799f1fa1b3ca88f5d9
David Axelrod <daxaxelrod@gmail.com>
2015-12-14 17:19:40 -0500

    final day of notes

---
+++ 12-14-15.py
@@ -0,0 +1,145 @@
+final day of class
+
+Real engines
+
+combustion engine (Otto cycle)
+
+expansion - gas air mix drawn into cylender
+
+piston compresses mixture
+
+blah blah
+
+heres how refridgeration works
+    heat engine running backards
+    uses the heat resevoir as a dump
+
+problem with perpetual motion machines
+    friction dissapates energy
+    violates the 2nd law of thermosynamics
+    extracting heat from a substance takes work
+
+
+
+REVEIWIWIWIWIWIWI
+
+review of fluids
+    Fluids
+    hydrostatic pressuse weight of water that sits on top
+    pressure = p not * desity of water * height of the water * gravity
+
+    Pascales principle
+        an applied pressure is felt equally throughout a closed containter. how hydrolic lifts work
+    archemedies principle
+        buoyant force is equal to the weight of the fluid that is displaced
+            how much of thr object is submerged? whats the weight of tge liquid for that volume
+
+    Bernoillis equaltion
+        TODO look up in textbook
+        can simplify quite a bit with cancelling
+
+thermodynamics
+    linear expansion = change in length = original length * expansion coefficiant (alpha) * delta temp
+    volume expansion = change in volume = original volume * beta (?!?!?!?!) * change in temp
+
+    specific heat
+        main idea is that its a proportionallity between how much heat exchange occurs when there is a temperatur change in a system
+        Q = c * mass * delta t
+        or
+        Q = c * moles * delta t
+        c is in joules / mol * kelvin
+
+        again what connect temp change and heat exchange
+
+        solve problems of different substances at different temps
+            they reach thermal equilibrium
+            Q1 + Q2 + Q3 ...... = 0
+            no net gain or loss of energy for system
+
+        during phase transition, not temp change
+            Q = L * mass
+            L = heat of transformation
+
+
+
+    WORK
+        area under pressure volume curve
+        work = intergral from volume initial to final of pressure * d volume
+        constant volume work = 0
+        constant pressure = p(vf - vi)
+        isothermal process work = nrt LN (Vf/ Vi) # for idea gas
+
+    First law of thermodynamics
+        internal energy
+        work is path dependent quantity
+        heat is path dependent
+        internal energy is not
+
+        delta e = Q - Work
+
+        for ideal gas
+            delta E = Cv * N * delta temp
+            can be used for all types of ideal gas things
+
+        for ideal gas again
+            if you dont know temp
+            pv = nrt
+            WRITE R ON INDEX CARD
+
+            Adiabatic means Q = 0
+            pv i gamma = pv gamma final so gamma equals Cp / Cv
+
+        Entropy
+            not going to be tested extensivly
+            entropy has to increase when there is an irreversable
+            entropy stays the same for reversable process
+
+            second law of thermo
+
+            CHANGE IN entropy = intergral from i to f of delta Q / Temp
+
+            for adiabatic, delta entropy = 0
+            for isothermal, Q/temp
+            for constant volume delta s = n Cv ln (Temp final/ Temp initial)
+
+            engines
+                carbot cycle
+                be able to calculate efficiancy for engine
+
+    Important theme of the course
+        MAIN SHIT
+        PAY THE FUCK ATTENTION TO THIS
+
+        decomposing vectors into components
+            forces, momentum
+
+        nmotion in 3d
+            projectile motion
+            forces and using force diagrams
+                anytime the problem has forces, drag a diagrams
+            mechanical energy and work
+                be able to calculate for when enrgy is conserved and not conserved
+                somtimes better to use energy rather than a force approach
+                also when objects change height
+            momentum
+                collisions (know different types of collisions)
+                consider momentum conservation in xyz directions
+            rotation
+            torque
+            angular momentum
+                be comforable with these
+                calculate sum of tourques
+                be able to connect translational momentum to rotaional
+            gravitation
+                be able to calculate force between 2 objects
+                be able to use spherical shell therum
+
+            and finally oscillations
+                a cos(x) type stuff
+                make sure to know equation for damped oscillations
+
+            questions can be from all over the coure so no help there
+
+
+            ---------------------------------------------------------------
+thats all folks
